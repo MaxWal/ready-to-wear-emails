60d6b1bc9823cb46b2d47e37ca93f4180136b9c7
Seafnox <kirill9990@gmail.com>
2014-04-07 19:23:46 +0400

    update style for microsoft stupid programm

--- order/index.php
+++ order/index.php
@@ -1,9 +1,11 @@
 <table id="wrapper" width="600" align="center" bgcolor="#FFFFFF" cellpadding="0" cellspacing="0" border="0" style="border-width:0;" >
     <tr style="font-family:Arial, Helvetica, sans-serif;" >
         <td class="content_wrapper" style="font-family:Arial, Helvetica, sans-serif;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" >
-            <table id="header" width="600" align="center" bgcolor="#000000" cellpadding="0" cellspacing="0" border="0" style="border-width:0;font-family:Arial, Helvetica, sans-serif;background-color:#000000;background-image:none;background-repeat:repeat;background-position:top left;background-attachment:scroll;font-size:12px;" ><tr style="font-family:Arial, Helvetica, sans-serif;" >
-                    <td class="icon" style="font-family:Arial, Helvetica, sans-serif;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;padding-top:12px;padding-bottom:12px;padding-right:0px;padding-left:0px;width:275px;text-align:center;" >
-                        <a href="//READY-TO-WEAR.RU" style="font-family:Arial, Helvetica, sans-serif;text-decoration:none;color:#ffffff;font-size:12px;display:block;text-indent:-9999px;overflow:hidden;background-image:url('images/logo.jpg');width:238px;height:27px;margin-top:0;margin-bottom:0;margin-right:auto;margin-left:auto;" >READY-TO-WEAR.RU</a>
+            <table id="header" width="600" align="center" bgcolor="#FFFFFF" cellpadding="0" cellspacing="0" border="0" style="border-width:0;font-family:Arial, Helvetica, sans-serif;background-color:#000000;background-image:none;background-repeat:repeat;background-position:top left;background-attachment:scroll;font-size:12px;" ><tr style="font-family:Arial, Helvetica, sans-serif;" >
+                    <td class="icon" width="275" style="font-family:Arial, Helvetica, sans-serif;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;padding-top:12px;padding-bottom:12px;padding-right:0px;padding-left:0px;width:275px;text-align:center;" >
+                        <a href="//READY-TO-WEAR.RU" style="font-family:Arial, Helvetica, sans-serif;text-decoration:none;color:#ffffff;font-size:12px;" >
+                            <img src="http://ready-emails.herokuapp.com/images/logo.jpg" alt="READY-TO-WEAR.RU" style="border:0;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;">
+                        </a>
                     </td>
                     <td class="menu" style="font-family:Arial, Helvetica, sans-serif;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;padding-top:12px;padding-bottom:12px;padding-right:0px;padding-left:0px;" ><a href="//ready-to-wear.ru" style="font-family:Arial, Helvetica, sans-serif;text-decoration:none;color:#ffffff;font-size:12px;" >Новинки</a></td>
                     <td class="menu" style="font-family:Arial, Helvetica, sans-serif;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;padding-top:12px;padding-bottom:12px;padding-right:0px;padding-left:0px;" ><a href="//ready-to-wear.ru" style="font-family:Arial, Helvetica, sans-serif;text-decoration:none;color:#ffffff;font-size:12px;" >Одежда</a></td>
@@ -16,20 +18,20 @@
     </tr>
     <tr style="font-family:Arial, Helvetica, sans-serif;" >
         <td class="content_wrapper" style="font-family:Arial, Helvetica, sans-serif;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;" >
-            <span class="header" style="font-family:Arial, Helvetica, sans-serif;display:block;margin-top:40px;margin-left:19px;font-size:16px;line-height:24px;width:350px;" >Уважаемый <strong style="font-family:Arial, Helvetica, sans-serif;" >Иванов Андрей</strong>, ваш заказ №124322 успешно оформлен!</span>
-            <span class="cart_header" style="display:block;margin-top:29px;margin-bottom:0;margin-right:20px;margin-left:20px;font-weight:700;font-size:15px;font-family:Arial, Helvetica, sans-serif;" >Cостав заказа:</span>
+            <p class="header" style="font-family:Arial, Helvetica, sans-serif;display:block;margin-top:40px;margin-left:19px;font-size:16px;line-height:24px;width:350px;" >Уважаемый <strong style="font-family:Arial, Helvetica, sans-serif;" >Иванов Андрей</strong>, ваш заказ №124322 успешно оформлен!</p>
+            <p class="cart_header" style="display:block;margin-top:29px;margin-bottom:0;margin-right:20px;margin-left:20px;font-weight:700;font-size:15px;font-family:Arial, Helvetica, sans-serif;" >Cостав заказа:</p>
 
-            <table id="cart" width="560" align="center" bgcolor="#FFFFFF" cellpadding="0" cellspacing="0" border="0" style="border-width:0;font-family:Arial, Helvetica, sans-serif;margin-top:28px;margin-bottom:20px;margin-right:20px;margin-left:20px;font-size:13px;" >
+            <table id="cart" width="560" align="center" bgcolor="#FFFFFF" cellpadding="0" cellspacing="0" border="0" style="border-width:0;font-family:Arial, Helvetica, sans-serif;margin-top:0px;margin-bottom:30px;margin-right:20px;margin-left:20px;font-size:13px;" >
                 <thead style="font-family:Arial, Helvetica, sans-serif;" ><tr style="font-family:Arial, Helvetica, sans-serif;" >
-                    <th style="font-family:Arial, Helvetica, sans-serif;font-weight:400;font-style:italic;font-size:11px;text-align:left;color:#666666;padding-bottom:5px;border-bottom-width:1px;border-bottom-style:solid;border-bottom-color:#CCC;" >Товар</th>
-                    <th style="font-family:Arial, Helvetica, sans-serif;font-weight:400;font-style:italic;font-size:11px;text-align:left;color:#666666;padding-bottom:5px;border-bottom-width:1px;border-bottom-style:solid;border-bottom-color:#CCC;" ></th>
-                    <th style="font-family:Arial, Helvetica, sans-serif;font-weight:400;font-style:italic;font-size:11px;text-align:left;color:#666666;padding-bottom:5px;border-bottom-width:1px;border-bottom-style:solid;border-bottom-color:#CCC;" >Размер</th>
-                    <th style="font-family:Arial, Helvetica, sans-serif;font-weight:400;font-style:italic;font-size:11px;text-align:left;color:#666666;padding-bottom:5px;border-bottom-width:1px;border-bottom-style:solid;border-bottom-color:#CCC;" >Цена</th>
-                    <th style="font-family:Arial, Helvetica, sans-serif;font-weight:400;font-style:italic;font-size:11px;text-align:left;color:#666666;padding-bottom:5px;border-bottom-width:1px;border-bottom-style:solid;border-bottom-color:#CCC;" >Итого</th>
+                    <th style="font-family:Arial, Helvetica, sans-serif;font-weight:400;font-style:italic;font-size:11px;text-align:left;color:#666666;padding-bottom:5px;padding-top:42px;border-bottom-width:1px;border-bottom-style:solid;border-bottom-color:#CCC;" >Товар</th>
+                    <th style="font-family:Arial, Helvetica, sans-serif;font-weight:400;font-style:italic;font-size:11px;text-align:left;color:#666666;padding-bottom:5px;padding-top:42px;border-bottom-width:1px;border-bottom-style:solid;border-bottom-color:#CCC;" >&nbsp;</th>
+                    <th style="font-family:Arial, Helvetica, sans-serif;font-weight:400;font-style:italic;font-size:11px;text-align:left;color:#666666;padding-bottom:5px;padding-top:42px;border-bottom-width:1px;border-bottom-style:solid;border-bottom-color:#CCC;" >Размер</th>
+                    <th style="font-family:Arial, Helvetica, sans-serif;font-weight:400;font-style:italic;font-size:11px;text-align:left;color:#666666;padding-bottom:5px;padding-top:42px;border-bottom-width:1px;border-bottom-style:solid;border-bottom-color:#CCC;" >Цена</th>
+                    <th style="font-family:Arial, Helvetica, sans-serif;font-weight:400;font-style:italic;font-size:11px;text-align:left;color:#666666;padding-bottom:5px;padding-top:42px;border-bottom-width:1px;border-bottom-style:solid;border-bottom-color:#CCC;" >Итого</th>
                 </tr></thead>
                 <tbody style="font-family:Arial, Helvetica, sans-serif;" >
                 <tr style="font-family:Arial, Helvetica, sans-serif;" >
-                    <td class="image" style="font-family:Arial, Helvetica, sans-serif;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;padding-bottom:0;padding-right:0;padding-left:0;padding-top:17px;width:100px;text-align:left;" ><img src="images/items/wear1.png" style="font-family:Arial, Helvetica, sans-serif;" ></td>
+                    <td class="image" style="font-family:Arial, Helvetica, sans-serif;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;padding-bottom:0;padding-right:0;padding-left:0;padding-top:17px;width:100px;text-align:left;" ><img src="http://ready-emails.herokuapp.com/images/items/wear1.png" style="font-family:Arial, Helvetica, sans-serif;" ></td>
                     <td class="info" style="font-family:Arial, Helvetica, sans-serif;padding-bottom:0;padding-right:0;padding-left:0;padding-top:17px;display:block;font-size:11px;line-height:16px;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;color:#666;width:170px;" >
                         <span class="name" style="font-family:Arial, Helvetica, sans-serif;font-size:13px;color:#000;font-weight:700;" >Alexis Mabille</span>
                         <br style="font-family:Arial, Helvetica, sans-serif;" >
@@ -40,7 +42,7 @@
                     <td class="total" style="font-family:Arial, Helvetica, sans-serif;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;padding-bottom:0;padding-right:0;padding-left:0;padding-top:17px;font-weight:700;" >21 500 p.</td>
                 </tr>
                 <tr style="font-family:Arial, Helvetica, sans-serif;" >
-                    <td class="image" style="font-family:Arial, Helvetica, sans-serif;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;padding-bottom:0;padding-right:0;padding-left:0;padding-top:17px;width:100px;text-align:left;" ><img src="images/items/wear3.png" style="font-family:Arial, Helvetica, sans-serif;" ></td>
+                    <td class="image" style="font-family:Arial, Helvetica, sans-serif;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;padding-bottom:0;padding-right:0;padding-left:0;padding-top:17px;width:100px;text-align:left;" ><img src="http://ready-emails.herokuapp.com/images/items/wear3.png" style="font-family:Arial, Helvetica, sans-serif;" ></td>
                     <td class="info" style="font-family:Arial, Helvetica, sans-serif;padding-bottom:0;padding-right:0;padding-left:0;padding-top:17px;display:block;font-size:11px;line-height:16px;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;color:#666;width:170px;" >
                         <span class="name" style="font-family:Arial, Helvetica, sans-serif;font-size:13px;color:#000;font-weight:700;" >Donna Karan</span>
                         <br style="font-family:Arial, Helvetica, sans-serif;" >
@@ -55,11 +57,11 @@
 
             <hr style="font-family:Arial, Helvetica, sans-serif;margin-top:0;margin-bottom:0;margin-right:20px;margin-left:20px;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;border-style:solid;border-top-width:0;border-color:#E9E9E9;" >
 
-            <table id="total" width="560" align="center" bgcolor="#FFFFFF" cellpadding="0" cellspacing="0" border="0" style="border-width:0;font-family:Arial, Helvetica, sans-serif;margin-top:40px;margin-bottom:0;margin-right:20px;margin-left:20px;font-size:11px;" >
+            <table id="total" width="560" align="center" bgcolor="#FFFFFF" cellpadding="0" cellspacing="0" border="0" style="border-width:0;font-family:Arial, Helvetica, sans-serif;margin-top:0px;margin-bottom:0;margin-right:20px;margin-left:20px;font-size:11px;" >
                 <thead style="font-family:Arial, Helvetica, sans-serif;" ><tr style="font-family:Arial, Helvetica, sans-serif;vertical-align:top;line-height:16px;" >
-                    <th style="font-family:Arial, Helvetica, sans-serif;text-align:left;font-size:14px;padding-bottom:20px;" >ДОСТАВКА</th>
-                    <th style="font-family:Arial, Helvetica, sans-serif;text-align:left;font-size:14px;padding-bottom:20px;" >ОПЛАТА</th>
-                    <th style="font-family:Arial, Helvetica, sans-serif;text-align:left;font-size:14px;padding-bottom:20px;" >ИТОГО</th>
+                    <th style="font-family:Arial, Helvetica, sans-serif;text-align:left;font-size:14px;padding-bottom:20px;padding-top:40px;">ДОСТАВКА</th>
+                    <th style="font-family:Arial, Helvetica, sans-serif;text-align:left;font-size:14px;padding-bottom:20px;padding-top:40px;">ОПЛАТА</th>
+                    <th style="font-family:Arial, Helvetica, sans-serif;text-align:left;font-size:14px;padding-bottom:20px;padding-top:40px;">ИТОГО</th>
                 </tr></thead>
                 <tbody style="font-family:Arial, Helvetica, sans-serif;" ><tr style="font-family:Arial, Helvetica, sans-serif;vertical-align:top;line-height:16px;" >
                     <td class="delivery" style="font-family:Arial, Helvetica, sans-serif;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;padding-top:0;padding-bottom:0;padding-right:0;padding-left:0;width:200px;" >
@@ -105,10 +107,10 @@
                     <td class="phone" style="font-family:Arial, Helvetica, sans-serif;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;padding-top:16px;padding-bottom:16px;padding-right:0px;padding-left:0px;width:140px;text-align:left;" >+7 (495) 609-69-68</td>
                     <td class="site" style="font-family:Arial, Helvetica, sans-serif;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;padding-top:16px;padding-bottom:16px;padding-right:0px;padding-left:0px;font-weight:700;width:143px;text-align:left;" ><a href="//ready-to-wear.ru" style="font-family:Arial, Helvetica, sans-serif;color:#ffffff;text-decoration:none;" >www.ready-to-wear.ru</a></td>
                     <td class="social" style="font-family:Arial, Helvetica, sans-serif;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;padding-top:16px;padding-bottom:16px;padding-left:0px;text-align:right;padding-right:10px;width:100px;" >
-                        <a href="//ready-to-wear.ru" class="fb" style="font-family:Arial, Helvetica, sans-serif;color:#ffffff;text-decoration:none;display:inline-block;width:19px;height:16px;background-image:url('images/fb.jpg');" ></a>
-                        <a href="//ready-to-wear.ru" class="vk" style="font-family:Arial, Helvetica, sans-serif;color:#ffffff;text-decoration:none;display:inline-block;width:19px;height:16px;background-image:url('images/vk.jpg');" ></a>
-                        <a href="//ready-to-wear.ru" class="tw" style="font-family:Arial, Helvetica, sans-serif;color:#ffffff;text-decoration:none;display:inline-block;width:19px;height:16px;background-image:url('images/tw.jpg');" ></a>
-                        <a href="//ready-to-wear.ru" class="ig" style="font-family:Arial, Helvetica, sans-serif;color:#ffffff;text-decoration:none;display:inline-block;background-image:url('images/ig.jpg');width:23px;height:16px;" ></a>
+                        <a href="//ready-to-wear.ru" class="fb" style="font-family:Arial, Helvetica, sans-serif;color:#ffffff;text-decoration:none;display:inline-block;width:19px;height:16px;background-image:url('http://ready-emails.herokuapp.com/images/fb.jpg');" ></a>
+                        <a href="//ready-to-wear.ru" class="vk" style="font-family:Arial, Helvetica, sans-serif;color:#ffffff;text-decoration:none;display:inline-block;width:19px;height:16px;background-image:url('http://ready-emails.herokuapp.com/images/vk.jpg');" ></a>
+                        <a href="//ready-to-wear.ru" class="tw" style="font-family:Arial, Helvetica, sans-serif;color:#ffffff;text-decoration:none;display:inline-block;width:19px;height:16px;background-image:url('http://ready-emails.herokuapp.com/images/tw.jpg');" ></a>
+                        <a href="//ready-to-wear.ru" class="ig" style="font-family:Arial, Helvetica, sans-serif;color:#ffffff;text-decoration:none;display:inline-block;background-image:url('http://ready-emails.herokuapp.com/images/ig.jpg');width:23px;height:16px;" ></a>
                     </td>
                 </tr></table>
         </td>
